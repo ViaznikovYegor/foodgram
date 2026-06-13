d0e12a1f344204f3e1f0e663e033c704d3a1254c
Viaznikov Yegor <dexbustakkaunt@gmail.com>
2026-06-13 21:11:14 +0300

    g

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -1,5 +1,6 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-// allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
-// learn more: https://github.com/testing-library/jest-dom
+// src/setupTests.js
 import '@testing-library/jest-dom';
+
+// Полифилл для clearImmediate / setImmediate
+global.setImmediate = (fn, ...args) => setTimeout(fn, 0, ...args);
+global.clearImmediate = (id) => clearTimeout(id);
